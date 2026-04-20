5b7de04d4dc225538cdd99e2eee11cd6ef60c14f
23f2004377 <23f2004377@ds.study.iitm.ac.in>
2026-04-20 10:30:00 +0530

    connecting to wifi

--- main.py
+++ main.py
@@ -5,3 +5,20 @@ import dht
 from machine import Pin
 
 sensor = dht.DHT11(Pin(4))
+
+# Connect to Wi-Fi
+ssid = 'xxxxxxxx'
+password = 'xxxxxxxx'
+REFRESH_SECONDS = 5
+
+wifi = network.WLAN(network.STA_IF)
+wifi.active(True)
+wifi.connect(ssid, password)
+
+start = time.time()
+while not wifi.isconnected():
+    if time.time() - start > 15:
+        raise RuntimeError('Wi-Fi connection timed out')
+    time.sleep(0.25)
+
+print('Connected. IP:', wifi.ifconfig()[0])
